6522484a448ffd4110244d57d78c0eb8f1cb025e
raveendra11 <Gurthuledu@1>
2025-03-08 04:39:02 -0600

    A program for Arrays Declaration and initialization.

---
+++ Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] rollNumber;
+
+        rollNumber = new int[5];
+
+        rollNumber[0] = 101;
+
+        int[] rollNumbers = new int[5];
+
+        rollNumbers[0] = 101;
+
+        int[] rollNo = new int[]{101, 102, 103, 104, 105};
+
+        int[] rollNum = {101, 102, 103, 104, 105};
+
+
+    }
+}

90925b05c68a3a28ffa988804856f9bd7c852a64
raveendra11 <Gurthuledu@1>
2025-03-09 06:42:04 -0500

    A program for Array to check smallest element.

---
+++ ArraySmallestElement.java
@@ -0,0 +1,22 @@
+public class ArraySmallestElement {
+
+    int marks[] = new int[]{95, 83, 68, 34, 100, 99, 90};
+
+    int min = marks[0];
+
+    public void findSmallest() {
+
+        for (int i = 1; i < marks.length; i++) {
+            if (marks[i] < min) {
+                min = marks[i];
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void main(String[] args) {
+
+        ArraySmallestElement ars = new ArraySmallestElement();
+        ars.findSmallest();
+    }
+}

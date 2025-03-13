ff6ac652e7007fcdfe838c5f4dbcc1c51115e27b
raveendra11 <Gurthuledu@1>
2025-03-13 15:43:14 -0500

    A program for Arrays to Rotate elements to left.

---
+++ ArrayRotateLeft.java
@@ -0,0 +1,25 @@
+public class ArrayRotateLeft {
+
+    int[] rollNumbers = new int[]{1, 2, 3, 4, 5, 6, 7};
+
+    int temp = rollNumbers[0];
+
+    public void doRotate() {
+        for (int i = 0; i < rollNumbers.length - 1; i++) {
+            rollNumbers[i] = rollNumbers[i + 1];
+        }
+        rollNumbers[rollNumbers.length - 1] = temp;
+
+        for (int k = 0; k < rollNumbers.length; k++) {
+            System.out.println(rollNumbers[k]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayRotateLeft art = new ArrayRotateLeft();
+        art.doRotate();
+
+    }
+}

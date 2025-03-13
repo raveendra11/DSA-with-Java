ca8490d414c8c4d38312bd3b6fb4753284bc3627
raveendra11 <Gurthuledu@1>
2025-03-12 22:50:55 -0500

    A Program for Array Reverse Traversal and Array traversal with forEach.

---
+++ ArrayReverseTraversal.java
@@ -0,0 +1,18 @@
+public class ArrayReverseTraversal {
+    int marks[] = new int[]{95, 68, 35, 43, 25, 65};
+
+    public void doReverse() {
+
+        for (int i = marks.length - 1; i >= 0; i--) {
+            System.out.println(marks[i]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayReverseTraversal art = new ArrayReverseTraversal();
+        art.doReverse();
+
+    }
+}

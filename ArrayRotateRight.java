7970c5ec1c055153fa7b8fadd163ee280ef037e7
raveendra11 <Gurthuledu@1>
2025-03-13 16:51:33 -0500

    A program for Arrays to Rotate elements to right.

---
+++ ArrayRotateRight.java
@@ -0,0 +1,28 @@
+public class ArrayRotateRight {
+
+    int rollNumbers[] = new int[]{1, 2, 3, 4, 5, 6};
+
+    int temp = rollNumbers[rollNumbers.length - 1];
+
+    public void doRotateRight() {
+
+        for (int i = rollNumbers.length - 1; i > 0; i--) {
+
+            rollNumbers[i] = rollNumbers[i - 1];
+
+
+        }
+
+        rollNumbers[0] = temp;
+
+        for (int j = 0; j < rollNumbers.length; j++) {
+            System.out.println(rollNumbers[j]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArrayRotateRight arr = new ArrayRotateRight();
+        arr.doRotateRight();
+    }
+}

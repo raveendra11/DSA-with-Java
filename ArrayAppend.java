369b1fce2bfdacc9fe0cd3b2378f58ada6d1b57f
raveendra11 <Gurthuledu@1>
2025-03-21 20:52:18 -0500

    A program for Arrays on adding an element at the end.

---
+++ ArrayAppend.java
@@ -0,0 +1,20 @@
+public class ArrayAppend {
+
+    public static void main(String[] args) {
+
+        int rollNumbers[] = new int[7];
+        int n = rollNumbers.length;
+        rollNumbers[0]=10;
+        rollNumbers[1]=20;
+        rollNumbers[2]=30;
+        rollNumbers[3]=40;
+        rollNumbers[4]=50;
+        rollNumbers[5]=60;
+        rollNumbers[n-1]=70;
+
+
+        for(int x : rollNumbers){
+            System.out.println(x);
+        }
+    }
+}

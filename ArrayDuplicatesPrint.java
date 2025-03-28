190ec1dc4d93cb35677ec983edbda5341ab1f2b2
raveendra11 <Gurthuledu@1>
2025-03-28 14:53:48 -0500

    A program for Arrays on printing duplicate elements .

---
+++ ArrayDuplicatesPrint.java
@@ -0,0 +1,23 @@
+public class ArrayDuplicatesPrint {
+
+    int[] numbers = new int[]{3, 6, 8, 8, 10, 12, 15, 15, 15, 20, 20, 20, 21, 21};
+
+    int duplicate;
+
+    public void printDuplicates() {
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == numbers[i + 1] && numbers[i + 1] != duplicate) {
+                duplicate = numbers[i];
+                System.out.println(duplicate);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ArrayDuplicatesPrint ard = new ArrayDuplicatesPrint();
+        ard.printDuplicates();
+
+    }
+}

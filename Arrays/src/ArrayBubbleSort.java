e602bd9e514a0166ec6568bef5a7aaa352d3ef43
raveendra11 <Gurthuledu@1>
2025-04-15 18:39:55 -0500

    All Array Programs are moved to Specific directory called Arrays.

--- Arrays/src/ArrayBubbleSort.java
+++ Arrays/src/ArrayBubbleSort.java
@@ -1,18 +1,17 @@
 public class ArrayBubbleSort {
 
-    int numbers[] = new int[]{1, 8, 5, 7, 3, 2,9};
+    int numbers[] = new int[]{1, 8, 5, 7, 3, 2, 9};
 
     public void doBubbleSort() {
 
         for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length-1-i; j++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
 
-                if(numbers[j]>numbers[j+1]){
-                    int temp = numbers[j+1];
-                    numbers[j+1]=numbers[j];
-                    numbers[j]=temp;
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j + 1];
+                    numbers[j + 1] = numbers[j];
+                    numbers[j] = temp;
                 }
-
             }
         }
         for (int k : numbers) {

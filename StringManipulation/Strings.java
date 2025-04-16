9d644d8f81622a3086808faeb1973ade4c10f239
raveendra11 <Gurthuledu@1>
2025-04-15 19:13:19 -0500

    A Program on Strings.

---
+++ StringManipulation/Strings.java
@@ -0,0 +1,11 @@
+package StringManipulation;
+
+public class Strings {
+
+    static String name ="ISHU";
+
+    public static void main(String[] args) {
+
+        System.out.println("How is life going "+name+ "?");
+    }
+}

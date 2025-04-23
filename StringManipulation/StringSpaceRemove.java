ab712a5818016ac6fd800f6ab6e501ab2e114cd4
raveendra11 <Gurthuledu@1>
2025-04-23 01:46:16 -0500

    A Program on Strings to remove whitespace between words(code updated to handle multiple whitespaces).

--- StringManipulation/StringSpaceRemove.java
+++ StringManipulation/StringSpaceRemove.java
@@ -2,24 +2,32 @@ package StringManipulation;
 
 public class StringSpaceRemove {
 
-    String msg = "Hey! Whatsup,ISHU?";
+    String msg = "Hey! Whatsup, ISHU?";
+
     char[] response = msg.toCharArray();
+
     int index;
 
     public void doSpaceRemove() {
 
-        for (int i = 0; i < response.length - 1; i++) {
+        int k = 0;
 
-            if (response[i] == ' ') {
-                index = i;
+        while (k < response.length) {
+
+            if (response[k] == ' ') {
+
+                index = k;
+
+                for (int j = index; j < response.length - 1; j++) {
+
+                    response[j] = response[j + 1];
+
+                }
+                response[response.length - 1] = ' ';
             }
+            k++;
         }
 
-        for (int j = index; j < response.length - 1; j++) {
-
-            response[j] = response[j + 1];
-
-        }
         for (int i = 0; i < response.length - 1; i++) {
 
             System.out.print(response[i]);
